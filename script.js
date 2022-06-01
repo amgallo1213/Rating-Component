0ae9da971d59ed7daa6dc281841fcf740d9e24d7
Andrea <90292543+amgallo1213@users.noreply.github.com>
2022-05-31 22:59:02 -0400

    updating css

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const numberCircle = document.querySelector(".number-circle");
+const numberCircle = document.querySelector(".number");
 
 function turnOrange() {
     numberCircle.style.color = "orange";
+}
+
+function submitRating() {
+    window.location.href="submitted.html";
 }
