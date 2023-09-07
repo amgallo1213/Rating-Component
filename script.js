a713b2dd50e3b957d928eeebed3effc08ab14339
Andrea <90292543+amgallo1213@users.noreply.github.com>
2023-09-07 17:02:36 -0400

    updating JS to fix onClick problem

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const numberCircle = document.querySelector(".number");
 
-function turnOrange(element) {
+
+function turnOrange(numberCircle) {
     numberCircle.style.backgroundColor = "orange";
     numberCircle.style.borderRadius = "50%";
     numberCircle.style.color = "white";
     numberCircle.style.borderColor = "orange";
 }
 
+
+
+
+
 function submitRating(element) {
-    window.location.href="submitted.html";
+    window.location.href = "submitted.html";
 }
+
